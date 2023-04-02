9b70b275c9f35583446d7739c68fe114c3c2c7cc
prosenjit12decm <97383726+prosenjit12decm@users.noreply.github.com>
2023-04-02 21:08:00 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+import Nabvar from './Components/Navbar';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleSubmit = () => {
+    // console.log(i);
+    // console.log(i);
+    // console.log(pass);
+    console.log('Valo hoini');
+  };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Nabvar name={name} />
+      Name :
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br />
+      Password :
+      <input
+        type="text"
+        value={pass}
+        onChange={(e) => {
+          setPass(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={handleSubmit}>submit</button>
+      {/* <Content/>
+      <Footer/> */}
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar({ name }) {
+  return (
+    <div>
+      This is navbar
+      <div>{name}</div>
+    </div>
+  );
+}
